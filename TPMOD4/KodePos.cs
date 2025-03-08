3bb8a0ee64af6af3554ac71dcca5eb8371fc2d45
yudhapakel <yudahp23@gmail.com>
2025-03-08 19:51:01 +0700

    initial commit

---
+++ TPMOD4/KodePos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD4
+{
+    internal class KodePos
+    {
+        private Dictionary<string, string> kodePosTable = new Dictionary<string, string>()
+    {
+        { "Jakarta", "10110" },
+        { "Bandung", "40111" },
+        { "Surabaya", "60111" },
+        { "Yogyakarta", "55111" }
+    };
+
+        public string GetKodePos(string kota)
+        {
+            if (kodePosTable.ContainsKey(kota))
+            {
+                return kodePosTable[kota];
+            }
+            return "Kode pos tidak ditemukan";
+        }
+    }
+}
---
+++ TPMOD4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TPMOD4;
+
+class Program
+{
+    static void Main()
+    {
+        // Tes KodePos
+        KodePos kodePos = new KodePos();
+        Console.Write("Masukkan nama kota: ");
+        string kota = Console.ReadLine();
+        Console.WriteLine($"Kode Pos {kota}: {kodePos.GetKodePos(kota)}");
+
+        Console.WriteLine("\n---\n");
+    }
+}
