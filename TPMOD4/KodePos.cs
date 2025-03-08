f3bb9c9ff650caada9556db4a4aad2703f4cc476
yudhapakel <yudahp23@gmail.com>
2025-03-08 20:06:20 +0700

    menambahkan implementasi dengan table-driven

--- TPMOD4/KodePos.cs
+++ TPMOD4/KodePos.cs
@@ -8,21 +8,25 @@ namespace TPMOD4
 {
     internal class KodePos
     {
-        private Dictionary<string, string> kodePosTable = new Dictionary<string, string>()
+        private static Dictionary<string, string> kodePosTable = new Dictionary<string, string>
     {
-        { "Jakarta", "10110" },
-        { "Bandung", "40111" },
-        { "Surabaya", "60111" },
-        { "Yogyakarta", "55111" }
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
     };
 
-        public string GetKodePos(string kota)
+
+        public static string GetKodePos(string kelurahan)
         {
-            if (kodePosTable.ContainsKey(kota))
-            {
-                return kodePosTable[kota];
-            }
-            return "Kode pos tidak ditemukan";
+            return kodePosTable.ContainsKey(kelurahan) ? kodePosTable[kelurahan] : "Kode pos tidak ditemukan";
         }
     }
 }
--- TPMOD4/Program.cs
+++ TPMOD4/Program.cs
@@ -5,11 +5,9 @@ class Program
 {
     static void Main()
     {
-        // Tes KodePos
-        KodePos kodePos = new KodePos();
-        Console.Write("Masukkan nama kota: ");
-        string kota = Console.ReadLine();
-        Console.WriteLine($"Kode Pos {kota}: {kodePos.GetKodePos(kota)}");
+        Console.Write("Masukkan nama kelurahan: ");
+        string kelurahan = Console.ReadLine();
+        Console.WriteLine($"Kode Pos {kelurahan}: {KodePos.GetKodePos(kelurahan)}");
 
         Console.WriteLine("\n---\n");
     }
